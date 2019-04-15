97265dd08ac14bcfa45cd2a59f74571dd4748c2b
PaschalisAthan <paschalis.athanasiadis.9648@studsent.uu.se>
2019-04-15 20:39:03 +0200

    Changed the whole workflow

--- experimental_scripts/db_targets.py
+++ experimental_scripts/db_targets.py
@@ -1,9 +1,11 @@
 import argparse
 import pandas as pd
+import os
 
 #"--input", type = file, help = "input filename"
 parser = argparse.ArgumentParser(description = 'Process the Chembl database')
-parser.add_argument('filename')
+parser.add_argument('filename', help = 'The database to take the targets from')
+parser.add_argument('output_folder', help = 'The folder to produce the target files')
 args = parser.parse_args()
 with open(args.filename, 'r') as file:
     df = pd.read_csv(file, delimiter = '\t', lineterminator = '\n', header = 0)
@@ -13,6 +15,7 @@ with open(args.filename, 'r') as file:
         df.iloc[i, 0] = df.iloc[i, 0].replace(' ','_')
         df.iloc[i, 0] = df.iloc[i, 0].replace('(','_')
         df.iloc[i, 0] = df.iloc[i, 0].replace(')','_')
+        df.iloc[i, 0] = df.iloc[i, 0].replace("'",'')
     for i in range(0, df.shape[0]):
         if df.iloc[i, 0] not in D:
             D[df.iloc[i, 0]] = []
@@ -34,7 +37,10 @@ with open(args.filename, 'r') as file:
             D.setdefault(df.iloc[j,0],[]).append('0\n')
         elif df.iloc[j,2]=='EC50' and df.iloc[j,3]<=10000:
             D.setdefault(df.iloc[j,0],[]).append('1\n')
+    os.mkdir(args.output_folder)
+    os.chdir(args.output_folder)
     for key,value in D.items():
         with open(key + '.json', 'w+') as f:
             f.write('SMILES' + '\t' + 'FLAG' + '\n')
             f.write(''.join(value))
+os.mkdir('only_non_binders')
--- experimental_scripts/workflow.go
+++ experimental_scripts/workflow.go
@@ -7,30 +7,17 @@ import (
 		
 func main() {
 
-wf1 := sp.NewWorkflow("DS_CPSign", 4)
+wf := sp.NewWorkflow("DS_CPSign", 4)
 
-pythonProc1 := wf1.NewProc("pythonProc1", "python3 ../db_targets.py ../database; echo 'done' > {o:donefile}")
-pythonProc1.SetOut("donefile", "log1.txt")
-wf1.Run()
+pythonProc1 := wf.NewProc("Proc1", "python3 ../db_targets.py ../database ../targets_folder; echo 'done' > {o:donefile}")
+targetsDirectory := spc.NewFileGlobberDependent(wf, "Targets_in_Dir", "./targets_folder/*.json")
+pythonProc2 := wf.NewProc("Proc2", "python3 ../balancing_targets.py {i:inpfiles} ../targets_folder; echo 'done' > {o:done2file}")
 
 
-
-wf2 := sp.NewWorkflow("DS_CPSign", 4)
-targetsDirectory := spc.NewFileGlobber(wf2, "Targets_in_Dir", "*.json")
-
-pythonProc2 := wf2.NewProc("pythonProc2", "python3 ../more_nonbinders.py {i:inpfiles}; echo 'done' > {o:done2file}")
+pythonProc1.SetOut("donefile", "log1")
+targetsDirectory.InDependency().From(pythonProc1.Out("donefile"))
 pythonProc2.In("inpfiles").From(targetsDirectory.Out())
-pythonProc2.SetOut("done2file", "log2.txt")
-wf2.Run()
+pythonProc2.SetOut("done2file", "{i:inpfiles|%.json}_done")
 
-
-
-wf3 := sp.NewWorkflow("DS_CPSign", 4)
-targetsDirectory2 := spc.NewFileGlobber(wf3, "Targets_in_Dir2", "*.json")
-
-pythonProc3 := wf3.NewProc("pythonProc3", "python3 ../more_binders.py {i:inpfiles2}; echo 'done' > {o:done3file}")
-pythonProc3.In("inpfiles2").From(targetsDirectory2.Out())
-pythonProc3.SetOut("done3file", "log3.txt")
-wf3.Run()
-
+wf.Run()
 }
