87aa528db9d8e2b3e6d3d5bc8cfaba203348e98c
Samuel Lampa <samuel.lampa@gmail.com>
2019-04-17 04:10:23 +0200

    Set and use explicit filenames for the db_targets.py script and in the workflow (donefile no longer needed)

--- experimental_scripts/workflow.go
+++ experimental_scripts/workflow.go
@@ -9,12 +9,12 @@ func main() {
 	wf := sp.NewWorkflow("DS_CPSign", 4)
 
 	// Create target datasets
-	createDatasets := wf.NewProc("create_datasets", "python3 ../db_targets.py ../database ../targets_folder; echo 'done' > {o:donefile}")
-	createDatasets.SetOut("donefile", "targets_folder.done")
+	createDatasets := wf.NewProc("create_datasets", "python3 ../db_targets.py --database=../database --outdir={o:outdir}")
+	createDatasets.SetOut("outdir", "targets_folder")
 
 	// Glob target datasets
 	globDatasets := spc.NewFileGlobberDependent(wf, "glob_datasets", "./targets_folder/*.json")
-	globDatasets.InDependency().From(createDatasets.Out("donefile"))
+	globDatasets.InDependency().From(createDatasets.Out("outdir"))
 
 	// Balance datasets
 	balanceDatasets := wf.NewProc("balance_datasets", "python3 ../balancing_targets.py {i:inpfiles} ../targets_folder; echo 'done' > {o:done2file}")
