924bb41572c63ac852bf1f5a88b5eaeba82c647a
Samuel Lampa <samuel.lampa@gmail.com>
2019-04-17 03:58:56 +0200

    Format, regroup and comment workflow code

--- experimental_scripts/workflow.go
+++ experimental_scripts/workflow.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-		sp "github.com/scipipe/scipipe"
-		spc "github.com/scipipe/scipipe/components"
-		)
-		
+	sp "github.com/scipipe/scipipe"
+	spc "github.com/scipipe/scipipe/components"
+)
+
 func main() {
+	wf := sp.NewWorkflow("DS_CPSign", 4)
 
-wf := sp.NewWorkflow("DS_CPSign", 4)
+	// Create target datasets
+	pythonProc1 := wf.NewProc("Proc1", "python3 ../db_targets.py ../database ../targets_folder; echo 'done' > {o:donefile}")
+	pythonProc1.SetOut("donefile", "log1")
 
-pythonProc1 := wf.NewProc("Proc1", "python3 ../db_targets.py ../database ../targets_folder; echo 'done' > {o:donefile}")
-targetsDirectory := spc.NewFileGlobberDependent(wf, "Targets_in_Dir", "./targets_folder/*.json")
-pythonProc2 := wf.NewProc("Proc2", "python3 ../balancing_targets.py {i:inpfiles} ../targets_folder; echo 'done' > {o:done2file}")
+	// Glob target datasets
+	targetsDirectory := spc.NewFileGlobberDependent(wf, "Targets_in_Dir", "./targets_folder/*.json")
+	targetsDirectory.InDependency().From(pythonProc1.Out("donefile"))
 
+	// Balance datasets
+	pythonProc2 := wf.NewProc("Proc2", "python3 ../balancing_targets.py {i:inpfiles} ../targets_folder; echo 'done' > {o:done2file}")
+	pythonProc2.In("inpfiles").From(targetsDirectory.Out())
+	pythonProc2.SetOut("done2file", "{i:inpfiles|%.json}_done")
 
-pythonProc1.SetOut("donefile", "log1")
-targetsDirectory.InDependency().From(pythonProc1.Out("donefile"))
-pythonProc2.In("inpfiles").From(targetsDirectory.Out())
-pythonProc2.SetOut("done2file", "{i:inpfiles|%.json}_done")
-
-wf.Run()
+	wf.Run()
 }
