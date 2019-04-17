1860f71827b8da1455842316651438b1030e5697
Samuel Lampa <samuel.lampa@gmail.com>
2019-04-17 04:38:30 +0200

    Use clearer naming of data folders

--- experimental_scripts/workflow.go
+++ experimental_scripts/workflow.go
@@ -10,17 +10,17 @@ func main() {
 
 	// Create target datasets
 	createDatasets := wf.NewProc("create_datasets", "python3 ../db_targets.py --database=../database --outdir={o:outdir}")
-	createDatasets.SetOut("outdir", "targets_folder")
+	createDatasets.SetOut("outdir", "all_targets")
 
 	// Glob target datasets
-	globDatasets := spc.NewFileGlobberDependent(wf, "glob_datasets", "./targets_folder/*.tsv")
+	globDatasets := spc.NewFileGlobberDependent(wf, "glob_datasets", "./all_targets/*.tsv")
 	globDatasets.InDependency().From(createDatasets.Out("outdir"))
 
 	// Balance datasets
 	balanceDatasets := wf.NewProc("balance_datasets", "python3 ../balancing_targets.py --dbfile=../database --infile={i:infile} --outfile={o:outfile} --nonbinders-dir={o:nonbinders_dir}")
 	balanceDatasets.In("infile").From(globDatasets.Out())
-	balanceDatasets.SetOut("outfile", "balanced_targets_folder/{i:infile|basename|%.tsv}.balanced.tsv")
-	balanceDatasets.SetOut("nonbinders_dir", "only_non_binders")
+	balanceDatasets.SetOut("outfile", "balanced_targets/{i:infile|basename|%.tsv}.balanced.tsv")
+	balanceDatasets.SetOut("nonbinders_dir", "only_non_binding_targets")
 
 	// Run workflow
 	wf.Run()
